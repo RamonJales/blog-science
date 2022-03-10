365ef4a3bcc9ca2a00b02c25ee10127d642d0493
RamonJales <ramonjales123@gmail.com>
2022-03-10 11:10:12 -0300

    getDetails-controller cretated and working

--- src/main/java/br/com/blog/controller/HomeController.java
+++ src/main/java/br/com/blog/controller/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import br.com.blog.model.dao.ArticleDao;
 
@@ -17,5 +18,11 @@ public class HomeController {
 	public String index(Model model) {
 		model.addAttribute("articleList", articleRepository.findAll());
 		return "/home/index";
+	}
+	
+	@GetMapping("/{id}")
+	public String getArticleDetails(Model model, @PathVariable("id") Integer id) {
+		model.addAttribute("article", articleRepository.findById(id));
+		return "/articles/articleDetails";
 	}
 }
